bd03f09dfa0516e5574d84d3a07fab5f69ef9018
kobimor15 <kobimor15@gmail.com>
2023-05-24 16:30:48 +0300

    Support creating a config file if not exists: enters local PC's IP address, and default port.

--- Common/FileConfig.cpp
+++ Common/FileConfig.cpp
@@ -8,17 +8,15 @@ namespace network_config
 	std::string get_local_ip() {
 		WSADATA wsaData;
 		if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-			std::cerr << "WSAStartup failed.\n";
+			std::cout << "Error - WSAStartup failed.\n";
 			return "error";
 		}
-
 		char hostname[256];
 		if (gethostname(hostname, sizeof(hostname)) == SOCKET_ERROR) {
-			std::cerr << "Failed to get the hostname.\n";
+			std::cout << "Error - Failed to get the hostname.\n";
 			WSACleanup();
 			return "error";
 		}
-
 		struct addrinfo* result = nullptr;
 		struct addrinfo* ptr = nullptr;
 		struct addrinfo hints;
@@ -28,7 +26,7 @@ namespace network_config
 		hints.ai_protocol = IPPROTO_TCP;
 
 		if (getaddrinfo(hostname, nullptr, &hints, &result) != 0) {
-			std::cerr << "Failed to get the address info.\n";
+			std::cout << "Error - Failed to get the address info.\n";
 			WSACleanup();
 			return "error";
 		}
@@ -46,6 +44,27 @@ namespace network_config
 		WSACleanup();
 	}
 
+	bool createNewConfigFile()
+	{
+		//Open config.cfg file for write:
+		std::ofstream configFileForWrite(configFileDefaultPath, std::ios::app);
+		if (!configFileForWrite.is_open()) {
+			std::cout << "Error - Failed to open the file for writing." << std::endl;
+			return false;
+		}
+		//Write local PC's ip and default port in it:
+		std::string serverIP = get_local_ip();
+		if (serverIP == "error")
+		{
+			std::cout << "Error - can't get the current PC's ip. enter it manually in config.cfg file.";
+			return false;
+		}
+		configFileForWrite << serverIP << std::endl;
+		configFileForWrite << DEFAULT_PORT;
+		configFileForWrite.close();
+		return true;
+	}
+
 	bool FileConfig::initFileConfig()
 	{
 		std::ifstream configFile(configFileDefaultPath);
@@ -56,39 +75,58 @@ namespace network_config
 			getline(configFile, serverPortString);
 			if (m_serverIP == "") //If the file is empty, get the current PC's ip, and put default port.
 			{
-				//Open config.cfg file for write:
-				std::ofstream configFileForWrite(configFileDefaultPath, std::ios::app);
-				if (!configFileForWrite.is_open()) {
-					std::cerr << "Failed to open the file for writing." << std::endl;
+				//Create config.cfg file and put the current PC's IP and a default port:
+				if (createNewConfigFile() == true)
+				{
+					std::ifstream configFile(configFileDefaultPath);
+					if (!configFile.good())
+					{
+						std::cout << "Error - Failed to open the file." << std::endl;
+						return false;
+					}
+					getline(configFile, m_serverIP);
+					getline(configFile, serverPortString);
+					m_serverPort = stoi(serverPortString);
+					return true; // No need to check validation, because it received manually.
+				}
+				else //Case of error in opening file or getting the IP
+					return false;
+			}
+			else //Case not empty content or not empty file - check validation
+			{
+				if (serverPortString.find_first_not_of("0123456789") != std::string::npos)
+				{
+					std::cout << "Error - port not made of digits only. Try to edit port.\n";
 					return false;
 				}
-				//Write local PC's ip and default port in it:
-				m_serverIP = get_local_ip();
-				if (m_serverIP == "error")
+				m_serverPort = stoi(serverPortString);
+				if (m_serverPort < 1024 || m_serverPort > 49151)
 				{
-					std::cout << "Error - can't get the current PC's ip. enter it manually in config.cfg file.";
+					std::cout << "Error - invalid port: port out of range. Try to edit port.\n";
+					return false;
 				}
-				configFileForWrite << m_serverIP << std::endl;
-				serverPortString = DEFAULT_PORT;
-				configFileForWrite << serverPortString;
-				configFileForWrite.close();
-			}
-			if (serverPortString.find_first_not_of("0123456789") != std::string::npos)
-			{
-				std::cout << "Error - port not made of digits only. Try to edit port.\n";
-				return false;
-			}
-			m_serverPort = stoi(serverPortString);
-			if (m_serverPort < 1024 || m_serverPort > 49151)
-			{
-				std::cout << "Error - invalid port: port out of range. Try to edit port.\n";
-				return false;
 			}
 		}
-		else
+		else //Case config file not found
 		{
-			std::cout << "Error - can't open config file.\n";
-			return false;
+			//Create config.cfg file and put the current PC's IP and a default port:
+			if (createNewConfigFile() == true)
+			{
+				std::ifstream configFile(configFileDefaultPath);
+				if (!configFile.good())
+				{
+					std::cout << "Error - Failed to open the file." << std::endl;
+					return false;
+				}
+				getline(configFile, m_serverIP);
+				getline(configFile, serverPortString);
+				m_serverPort = stoi(serverPortString);
+			}
+			else //Case of error in opening file or getting the IP
+			{
+				std::cout << "Error - Can't create config file.\n";
+				return false;
+			}
 		}
 		return true;
 	}
